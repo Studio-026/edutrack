e7c1aa61a0fe72e6a045775596ddcc229c9cab41
Basel <basel.ismail008@gmail.com>
2026-09-15 23:20:56 +0300

    Update tests_questions.py
    Co-authored-by: Mahmoud Hesham Mahmoud Ibrahim Elkholany <mahmoud.dev.ai@gmail.com>

--- tests_questions.py
+++ tests_questions.py
@@ -100,31 +100,3 @@ def delete_test(test_id: int, username: str, password: str):
         raise HTTPException(status_code=403, detail="Students cannot delete tests")
 
     
-# @router.post("/submissions")
-# def submit_test(username: str, password: str, submission: Submission):
-#     user = login(username, password)
-#     specific_test = None
-#     score = 0
-
-#     if not user:
-#         raise HTTPException(status_code=401, detail="Invalid login")
-        
-#     if user.role != "student":
-#         raise HTTPException(status_code=403, detail="Only students can submit tests")
-        
-#     for t in tests:
-#         if t.id == submission.test_id:
-#             specific_test = t
-#             break
-
-#     if not specific_test:
-#         raise HTTPException(status_code=404, detail="Test not found")
-        
-#     for question in specific_test.questions:
-#         if submission.student_answers.get(question.id) == question.correct_answer:
-#             score += 1
-            
-#     submission.score = score
-#     submissions.append(submission)
-    
-#     return submission
